ea9f9ebcff4c9dafc5da1d7dd89ea5d9d232c894
zeeshan <zeeshanhamdani98@gmail.com>
2023-02-24 18:17:02 +0500

    vote ts test cases

--- test/vote.test.ts
+++ test/vote.test.ts
@@ -13,10 +13,15 @@ describe("Vote", function () {
 		otherAccount: SignerWithAddress,
 		fundsReciever: SignerWithAddress,
 		buyer: SignerWithAddress,
-		royaltyGetter: SignerWithAddress;
+		royaltyGetter: SignerWithAddress,
+		redCaptain: SignerWithAddress,
+		greenCaptain: SignerWithAddress,
+		yellowCaptain: SignerWithAddress,
+		blueCaptain: SignerWithAddress
+		;
 
 	before("Deployment and variables setup", async () => {
-		[deployer, otherAccount, fundsReciever, buyer, royaltyGetter] =
+		[deployer, otherAccount, fundsReciever, buyer, royaltyGetter, redCaptain, greenCaptain, yellowCaptain, blueCaptain] =
 			await ethers.getSigners();
 
 		const Token = await ethers.getContractFactory("WeRplay");
@@ -49,6 +54,64 @@ describe("Vote", function () {
 			).to.be.equal(true);
 		});
 
+		it("Vote contract admin has intialiazed roles", async function () {
+			let hasRoles = await Promise.all([
+				vote.hasRole(await vote.DEFAULT_ADMIN_ROLE(), deployer.address),
+				vote.hasRole(await vote.TECH_ADMIN(), deployer.address),
+				vote.hasRole(await vote.TOKEN_ADMIN(), deployer.address),
+			])
+
+			hasRoles.map((e) => expect(e).to.be.equal(true));
+		});
+
+		it("Test setup cohort all cohorts", async function () {
+			// Creates a normal cohort
+
+			let cohortId = 0;
+			await vote.setupCohort(cohortId++, "Red", redCaptain.address);
+			await vote.setupCohort(cohortId++, "Blue", blueCaptain.address);
+			await vote.setupCohort(cohortId++, "Green", greenCaptain.address);
+			await vote.setupCohort(cohortId, "Yellow", yellowCaptain.address);
+
+			let totalCohorts = await vote.totalCohorts();
+			expect(totalCohorts).to.equal(cohortId + 1);
+
+		})
+
+		it("Reverts: Cohort id more than counter", async function () {
+			let cohortId = 10;
+
+			// Create a new cohort with id < 0;
+			await expect(vote.setupCohort(cohortId, "Green", greenCaptain.address))
+				.to
+				.be
+				.revertedWith("Cohort Id not valid");
+		})
+
+		it("Reverts: Duplicate id 0 ", async function () {
+			let cohortId = 0;
+			await expect(vote.setupCohort(cohortId, "Redd", redCaptain.address)).to.be.revertedWith("Cohort Already set");
+		})
+
+		it("Reverts: Setup Cohort called by unauthorized role.", async function () {
+			await expect(
+				vote.connect(otherAccount).setupCohort(1, "Green", greenCaptain.address)
+			).to.be.revertedWith(
+				`AccessControl: account ${otherAccount.address.toLowerCase()} is missing role ${await vote.TECH_ADMIN()}`
+			);
+		})
+
+		it("Cohort name and id is set correctly", async function () {
+			let cohortId = 0;
+			let cohortName = "Red";
+			let admin = redCaptain.address;
+
+			let cohort = await vote.cohortMap(cohortId);
+
+			expect(cohort.name).to.be.equal(cohortName);
+			expect(cohort.admin).to.be.equal(admin);
+		});
+
 		// 	it("saleActive is false", async function () {
 		// 		expect(await jambroSale.saleActive()).to.be.equal(false);
 		// 	});
@@ -63,6 +126,55 @@ describe("Vote", function () {
 		// 		expect(await jambroSale.royalty()).to.be.equal(royalty);
 		// 	});
 	});
+
+	describe("Proposals", async function () {
+		it("Test only cohort admin can create proposal", async function () {
+			await vote.connect(redCaptain)
+				.makeProposal(0, "Test Objective for red cohort", 30);
+
+			expect(await (await vote.proposalMap(0)).exists)
+				.to
+				.be
+				.equal(true);
+		})
+		it("Reverts: Captain of another cohort can not create propsals in another cohort", async function () {
+			let timeFrame = 3 * 60 * 60;
+			await expect(vote.connect(greenCaptain)
+				.makeProposal(1, "Test Objective for red cohort", timeFrame)).revertedWith('Not Cohort Admin');
+		})
+
+		it("Proposal owner voted with in timeframe", async function () {
+			await vote.connect(redCaptain).vote(0, 0);
+			let hasVoted = await vote.proposalVotingMap(0, 0);
+			expect(hasVoted).to.be.equal(true);
+		})
+
+		it("Anyother cohort captain vote with in timeframe", async function () {
+			await vote.connect(greenCaptain).vote(0, 2);
+
+			let hasVoted = await vote.proposalVotingMap(0, 2);
+			expect(hasVoted).to.be.equal(true);
+		})
+
+		it("Reverts: Voting result not available will still voting", async function () {
+			await expect(vote.resultOfProposal(0)).revertedWith("Voting Still Active");
+		})
+		it("Forwarding block chain time", async function () {
+			let blockChainTime = await time.latest();
+			let newBlockChainTime = await time.increase(4 * 3600);
+			expect(newBlockChainTime).to.be.equal(blockChainTime + (4 * 3600));
+		})
+
+		it("Returns votes after voting ended", async function () {
+			let proposalResults = await vote.resultOfProposal(0);
+			expect(proposalResults).to.be.equal(2);
+		})
+
+		it("Reverts: Voting Not Active", async function () {
+			await expect(vote.connect(yellowCaptain).vote(0, 3))
+				.revertedWith("Voting Not Active");
+		});
+	})
 
 	// describe("Sale Setup Test", function () {
 	// 	it("Reverts : setting up NFT for sale when Sale is not Active", async function () {
