8d338b63d1164a0628e98a363474d7e7e367b8d2
Mueed <ab.mueed@werplay.com>
2023-02-28 11:36:10 +0500

    test cases

--- test/vote.test.ts
+++ test/vote.test.ts
@@ -10,14 +10,29 @@ describe("Vote", function () {
 	let token: WeRplay;
 	let vote: WrpVote;
 	let deployer: SignerWithAddress,
-		zero: SignerWithAddress,
-		one: SignerWithAddress,
-		two: SignerWithAddress,
-		three: SignerWithAddress,
-		four: SignerWithAddress;
+		otherAccount: SignerWithAddress,
+		fundsReciever: SignerWithAddress,
+		buyer: SignerWithAddress,
+		royaltyGetter: SignerWithAddress,
+		redCaptain: SignerWithAddress,
+		greenCaptain: SignerWithAddress,
+		yellowCaptain: SignerWithAddress,
+		blueCaptain: SignerWithAddress,
+		testNewAdmin: SignerWithAddress;
 
 	before("Deployment and variables setup", async () => {
-		[deployer, zero, one, two, three, four] = await ethers.getSigners();
+		[
+			deployer,
+			otherAccount,
+			fundsReciever,
+			buyer,
+			royaltyGetter,
+			redCaptain,
+			greenCaptain,
+			yellowCaptain,
+			blueCaptain,
+			testNewAdmin,
+		] = await ethers.getSigners();
 
 		const Token = await ethers.getContractFactory("WeRplay");
 		const Vote = await ethers.getContractFactory("wrpVote");
@@ -48,175 +63,241 @@ describe("Vote", function () {
 				await token.hasRole(await token.TOKEN_ADMIN(), vote.address)
 			).to.be.equal(true);
 		});
-	});
 
-	describe("setupCohort Test", function () {
-		it("sets up 5 cohorts", async function () {
-			const admins = [
-				zero.address,
-				one.address,
-				two.address,
-				three.address,
-				four.address,
-			];
+		it("Vote contract admin has intialiazed roles", async function () {
+			let hasRoles = await Promise.all([
+				vote.hasRole(await vote.DEFAULT_ADMIN_ROLE(), deployer.address),
+				vote.hasRole(await vote.TECH_ADMIN(), deployer.address),
+				vote.hasRole(await vote.TOKEN_ADMIN(), deployer.address),
+			]);
 
-			for (let i = 0; i < 5; i++) {
-				await vote.setupCohort(i, `Cohort ${i}`, admins[i]);
-				const res = await vote.cohortMap(i);
-				expect(res.name).to.be.equal(`Cohort ${i}`);
-				expect(res.admin).to.be.equal(admins[i]);
-				expect(res.exists).to.be.equal(true);
-				expect(await vote.totalCohorts()).to.be.equal(i + 1);
-			}
+			hasRoles.map((e) => expect(e).to.be.equal(true));
 		});
 
-		// it("Forwards blockchain to active sale time", async function () {
-		// 	await time.increase(ONE_DAY_IN_SECS);
-		// });
+		it("Test setup cohort all cohorts", async function () {
+			// Creates a normal cohort
 
-		// it("Reverts : setting up NFT for sale with price 0", async function () {
-		// 	await expect(
-		// 		jambroSale.setForSale(1, ethers.utils.parseEther("0"))
-		// 	).to.be.revertedWith("Price cannot be 0");
-		// });
+			let cohortId = 0;
+			// 0
+			await vote.setupCohort(cohortId++, "Red", redCaptain.address);
+			// 1
+			await vote.setupCohort(cohortId++, "Blue", blueCaptain.address);
+			// 2
+			await vote.setupCohort(cohortId++, "Green", greenCaptain.address);
+			// 3
+			await vote.setupCohort(cohortId, "Yellow", yellowCaptain.address);
 
-		// it("Reverts : setting up NFT for sale not minted yet", async function () {
-		// 	await expect(
-		// 		jambroSale.setForSale(1, ethers.utils.parseEther("1"))
-		// 	).to.be.revertedWith("ERC721: invalid token ID");
-		// });
+			let totalCohorts = await vote.totalCohorts();
+			expect(totalCohorts).to.equal(cohortId + 1);
+		});
 
-		// it("Mints id 0 to deployer and 1 to otherAccount", async function () {
-		// 	await jambro.safeMint(deployer.address, "uri For Id 0");
-		// 	await jambro.safeMint(otherAccount.address, "uri For Id 1");
+		it("Reverts: Cohort id more than counter", async function () {
+			let cohortId = 10;
 
-		// 	expect(await jambro.deployerOf(0)).to.be.equal(deployer.address);
+			// Create a new cohort with id < 0;
+			await expect(
+				vote.setupCohort(cohortId, "Green", greenCaptain.address)
+			).to.be.revertedWith("Cohort Id not valid");
+		});
 
-		// 	expect(await jambro.deployerOf(1)).to.be.equal(otherAccount.address);
-		// });
+		it("Reverts: Duplicate id 0 ", async function () {
+			let cohortId = 0;
+			await expect(
+				vote.setupCohort(cohortId, "Redd", redCaptain.address)
+			).to.be.revertedWith("Cohort Already set");
+		});
 
-		// it("Reverts : setting up NFT for sale not owned by caller", async function () {
-		// 	await expect(
-		// 		jambroSale.setForSale(1, ethers.utils.parseEther("1"))
-		// 	).to.be.revertedWith("Address not deployer of this token Id");
-		// });
+		it("Reverts: Setup Cohort called by unauthorized role.", async function () {
+			await expect(
+				vote.connect(otherAccount).setupCohort(1, "Green", greenCaptain.address)
+			).to.be.revertedWith(
+				`AccessControl: account ${otherAccount.address.toLowerCase()} is missing role ${await vote.TECH_ADMIN()}`
+			);
+		});
 
-		// it("Reverts : setting up NFT for sale but no allowance to sale contract", async function () {
-		// 	await expect(
-		// 		jambroSale
-		// 			.connect(otherAccount)
-		// 			.setForSale(1, ethers.utils.parseEther("1"))
-		// 	).to.be.revertedWith("Sale Contract not approved for this NFT");
-		// });
+		it("Cohort name and id is set correctly", async function () {
+			let cohortId = 0;
+			let cohortName = "Red";
+			let admin = redCaptain.address;
 
-		// it("sets up nft 1 for sale with price 2 eth", async function () {
-		// 	await jambro.connect(otherAccount).approve(jambroSale.address, 1);
-		// 	await jambroSale
-		// 		.connect(otherAccount)
-		// 		.setForSale(1, ethers.utils.parseEther("2"));
+			let cohort = await vote.cohortMap(cohortId);
 
-		// 	const price = ethers.utils.parseEther("2");
-		// 	expect((await jambroSale.nftMap(1)).price).to.be.equal(price);
-		// });
+			expect(cohort.name).to.be.equal(cohortName);
+			expect(cohort.admin).to.be.equal(admin);
+		});
 
-		// it("updates nft 1 for sale with price 2.5 eth", async function () {
-		// 	await jambro.connect(otherAccount).approve(jambroSale.address, 1);
-		// 	await jambroSale
-		// 		.connect(otherAccount)
-		// 		.setForSale(1, ethers.utils.parseEther("2.5"));
+		// 	it("saleActive is false", async function () {
+		// 		expect(await jambroSale.saleActive()).to.be.equal(false);
+		// 	});
 
-		// 	const price = ethers.utils.parseEther("2.5");
-		// 	expect((await jambroSale.nftMap(1)).price).to.be.equal(price);
-		// });
+		// 	it("royaltyGetter is right", async function () {
+		// 		expect(await jambroSale.royaltyGetter()).to.be.equal(
+		// 			royaltyGetter.address
+		// 		);
+		// 	});
 
-		// it("sale jambro Commission is right", async function () {
-		// 	const result = await jambroSale
-		// 		.connect(buyer)
-		// 		.jambroCommission(ethers.utils.parseEther("2.5"));
-
-		// 	expect(ethers.utils.formatEther(result)).to.be.equal("0.025");
-		// });
-
-		// it("setting commission to 10%", async function () {
-		// 	await jambroSale
-		// 		.connect(deployer)
-		// 		.setRoyalty(ethers.utils.parseEther("0.1"));
-		// });
-
-		// it("sale jambro Commission is right", async function () {
-		// 	const result = await jambroSale
-		// 		.connect(buyer)
-		// 		.jambroCommission(ethers.utils.parseEther("2.5"));
-
-		// 	expect(ethers.utils.formatEther(result)).to.be.equal("0.25");
-		// });
+		// 	it("royalty is right", async function () {
+		// 		expect(await jambroSale.royalty()).to.be.equal(royalty);
+		// 	});
 	});
 
-	// describe("Buy Test", function () {
-	// 	it("Reverts : buys a nft not for sale", async function () {
-	// 		await expect(jambroSale.connect(buyer).buy(0)).to.be.rejectedWith(
-	// 			"NFT not for Sale"
-	// 		);
-	// 	});
+	describe("Proposals", async function () {
+		it("Test only cohort admin can create proposal", async function () {
+			await vote
+				.connect(redCaptain)
+				.makeProposal(0, "Test Objective for red cohort", 30);
 
-	// 	it("Reverts : buys a nft with value not equal to price", async function () {
-	// 		await expect(
-	// 			jambroSale
-	// 				.connect(buyer)
-	// 				.buy(1, { value: ethers.utils.parseEther("2") })
-	// 		).to.be.rejectedWith("Value not equal to sale price");
-	// 	});
+			expect(await (await vote.proposalMap(0)).exists).to.be.equal(true);
+		});
+		it("Reverts: Captain of another cohort can not create propsals in another cohort", async function () {
+			let timeFrame = 3 * 60 * 60;
+			await expect(
+				vote
+					.connect(greenCaptain)
+					.makeProposal(1, "Test Objective for red cohort", timeFrame)
+			).revertedWith("Not Cohort Admin");
+		});
 
-	// 	it("Sets nft not for sale", async function () {
-	// 		await jambroSale.connect(otherAccount).removeFromSale(1);
-	// 	});
+		it("Proposal owner voted with in timeframe", async function () {
+			await vote.connect(redCaptain).vote(0, 0);
+			let hasVoted = await vote.proposalVotingMap(0, 0);
+			expect(hasVoted).to.be.equal(true);
+		});
 
-	// 	it("Reverts : buys a nft", async function () {
-	// 		await expect(
-	// 			jambroSale
-	// 				.connect(buyer)
-	// 				.buy(1, { value: ethers.utils.parseEther("2.5") })
-	// 		).to.be.rejectedWith("NFT not for Sale");
-	// 	});
+		it("Anyother cohort captain vote with in timeframe", async function () {
+			await vote.connect(greenCaptain).vote(0, 2);
 
-	// 	it("sets up nft 1 for sale with price 2.5 eth", async function () {
-	// 		await jambro.connect(otherAccount).approve(jambroSale.address, 1);
-	// 		await jambroSale
-	// 			.connect(otherAccount)
-	// 			.setForSale(1, ethers.utils.parseEther("2.5"));
+			let hasVoted = await vote.proposalVotingMap(0, 2);
+			expect(hasVoted).to.be.equal(true);
+		});
 
-	// 		const price = ethers.utils.parseEther("2.5");
-	// 		expect((await jambroSale.nftMap(1)).price).to.be.equal(price);
-	// 	});
+		it("Reverts: Voting result not available will still voting", async function () {
+			await expect(vote.resultOfProposal(0)).revertedWith(
+				"Voting Still Active"
+			);
+		});
+		it("Forwarding block chain time", async function () {
+			let blockChainTime = await time.latest();
+			let newBlockChainTime = await time.increase(4 * 3600);
+			expect(newBlockChainTime).to.be.equal(blockChainTime + 4 * 3600);
+		});
 
-	// 	it("buys a nft", async function () {
-	// 		await jambroSale
-	// 			.connect(buyer)
-	// 			.buy(1, { value: ethers.utils.parseEther("2.5") });
-	// 	});
+		it("Returns votes after voting ended", async function () {
+			let proposalResults = await vote.resultOfProposal(0);
+			expect(proposalResults).to.be.equal(2);
+		});
 
-	// 	it("NFT 1 is no longer for sale", async function () {
-	// 		expect((await jambroSale.connect(buyer).nftMap(1)).forSale).to.be.equal(
-	// 			false
-	// 		);
+		it("Reverts: Voting Not Active", async function () {
+			await expect(vote.connect(yellowCaptain).vote(0, 3)).revertedWith(
+				"Voting Not Active"
+			);
+		});
+	});
 
-	// 		expect((await jambroSale.connect(buyer).nftMap(1)).price).to.be.equal(0);
+	describe("Change Default Admin", async function () {
+		it("Reverts: Not Cohort Admin", async function () {
+			await expect(
+				vote.connect(otherAccount).changeDefaultAdmin(testNewAdmin.address, 0)
+			).revertedWith("Not Cohort Admin");
+		});
+		it("Vote casted by a cohort admin", async function () {
+			let totalCohorts = await vote.totalCohorts();
+			let totalVotes = 0;
+			for (let i = 0; i < totalCohorts.toBigInt(); i++) {
+				if (await vote.changeDefaultAdminMap(testNewAdmin.address, i))
+					totalVotes++;
+			}
 
-	// 		expect((await jambroSale.connect(buyer).nftMap(1)).deployer).to.be.equal(
-	// 			ethers.constants.AddressZero
-	// 		);
-	// 	});
+			await vote
+				.connect(redCaptain)
+				.changeDefaultAdmin(testNewAdmin.address, 0);
 
-	// 	it("previous deployer gets sale funds from buyer", async function () {
-	// 		const balance = ethers.utils.formatEther(await otherAccount.getBalance());
-	// 		expect(parseFloat(balance).toFixed(2)).to.be.equal("10002.25");
-	// 	});
+			let newTotalVotes = 0;
+			for (let i = 0; i < totalCohorts.toBigInt(); i++) {
+				if (await vote.changeDefaultAdminMap(testNewAdmin.address, i))
+					newTotalVotes++;
+			}
 
-	// 	it("royalty getter gets royalty funds", async function () {
-	// 		const balance = ethers.utils.formatEther(
-	// 			await royaltyGetter.getBalance()
-	// 		);
-	// 		expect(parseFloat(balance).toFixed(2)).to.be.equal("10000.25");
-	// 	});
-	// });
+			expect(totalVotes).to.be.equal(0);
+			expect(newTotalVotes).to.be.equal(1);
+		});
+
+		it("Default admin changes at half or more cohorts vote", async function () {
+			expect(
+				await vote.hasRole(
+					await vote.DEFAULT_ADMIN_ROLE(),
+					testNewAdmin.address
+				)
+			).to.be.equal(false);
+
+			await vote
+				.connect(greenCaptain)
+				.changeDefaultAdmin(testNewAdmin.address, 2);
+			await vote
+				.connect(yellowCaptain)
+				.changeDefaultAdmin(testNewAdmin.address, 3);
+
+			expect(
+				await vote.hasRole(
+					await vote.DEFAULT_ADMIN_ROLE(),
+					testNewAdmin.address
+				)
+			).to.be.equal(true);
+		});
+
+		it("Clear Default Admin Map After Voting is done", async function () {
+			let isEmpty = false;
+			let totalCohorts = await vote.totalCohorts();
+
+			for (let i = 0; i < totalCohorts.toNumber(); i++) {
+				if (await vote.changeDefaultAdminMap(testNewAdmin.address, i)) {
+					isEmpty = true;
+					break;
+				}
+			}
+			expect(isEmpty).to.be.equal(false);
+		});
+	});
+
+	describe("Change Cohort Admin", async function () {
+		it("Reverts: Not Cohort Admin", async function () {
+			await expect(
+				vote.connect(otherAccount).changeCohortAdmin(testNewAdmin.address, 0, 2)
+			).revertedWith("Not Cohort Admin");
+		});
+		it("Reverts: Current Admin of Cohort cannot vote", async function () {
+			await expect(
+				vote.connect(redCaptain).changeCohortAdmin(testNewAdmin.address, 0, 0)
+			).revertedWith("Current Admin of Cohort cannot vote");
+		});
+
+		it("Half or more votes changes the cohort admin.", async function () {
+			expect((await vote.cohortMap(0)).admin).to.be.equal(redCaptain.address);
+
+			await vote
+				.connect(blueCaptain)
+				.changeCohortAdmin(testNewAdmin.address, 1, 0);
+			await vote
+				.connect(greenCaptain)
+				.changeCohortAdmin(testNewAdmin.address, 2, 0);
+			await vote
+				.connect(yellowCaptain)
+				.changeCohortAdmin(testNewAdmin.address, 3, 0);
+
+			expect((await vote.cohortMap(0)).admin).to.be.equal(testNewAdmin.address);
+		});
+
+		it("Clear Cohort Admin Map After Voting is done", async function () {
+			let isEmpty = false;
+			let totalCohorts = await vote.totalCohorts();
+
+			for (let i = 0; i < totalCohorts.toNumber(); i++) {
+				if (await vote.changeCohortAdminMap(0, testNewAdmin.address, i)) {
+					isEmpty = true;
+					break;
+				}
+			}
+			expect(isEmpty).to.be.equal(false);
+		});
+	});
 });
